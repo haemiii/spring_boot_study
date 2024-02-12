850c289bcdcf1045807417c6f6c9f21c58258081
강혜미 <eham1003@naver.com>
2024-02-12 16:35:07 +0900

    password encoding

--- src/test/java/com/study/spring_web/account/AccountControllerTest.java
+++ src/test/java/com/study/spring_web/account/AccountControllerTest.java
@@ -1,5 +1,6 @@
 package com.study.spring_web.account;
 
+import com.study.spring_web.domain.Account;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -62,7 +63,9 @@ class AccountControllerTest {
                 .andExpect(status().is3xxRedirection())
                 .andExpect(view().name("redirect:/"));
 
-        assertTrue(accountRepository.existsByEmail("cccc@naver.com"));
+        Account account = accountRepository.findByEmail("cccc@naver.com");
+        assertNotNull(account);
+        assertNotEquals(account.getPassword(), "12345678");
         then(javaMailSender).should().send(any(SimpleMailMessage.class));
     }
 }
