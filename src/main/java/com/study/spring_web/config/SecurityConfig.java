e05798c178a960f81b0e0def89e3257939c4ac91
강혜미 <eham1003@naver.com>
2024-02-12 14:51:05 +0900

    sign-up page

---
+++ src/main/java/com/study/spring_web/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.study.spring_web.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@EnableWebSecurity  // 시큐리티 설정을 직접 하겠다
+@EnableWebMvc
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        // 개발시에 필요한 몇몇 Url만 허용
+        // profile은 GET요청만 허용
+        http
+                .authorizeHttpRequests((auth) -> auth
+                        .requestMatchers("/", "/login", "/sign-up", "/check-email", "/check-email-token",
+                                "/email-login", "/check-email-login", "/login-link").permitAll()
+                        .requestMatchers(HttpMethod.GET, "/profile/*").permitAll()
+                        .anyRequest().authenticated()
+                );
+        return http.build();
+
+    }
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
+    }
+}
---
+++ src/main/java/com/study/spring_web/domain/Account.java
@@ -0,0 +1,58 @@
+package com.study.spring_web.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter @Setter
+@EqualsAndHashCode(of="id")
+@Builder @AllArgsConstructor @NoArgsConstructor
+// entity : primary key를 추가해라
+// id : 연관관계가 복잡해질 때 이꼴앤해시코드에서 서로다른 연관관계를 계속해서
+// 순환참조 하느라 무한 루프가 발생하고 결국에는 스택오버플로우 발생
+public class Account {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column(unique = true)
+    private String nickname;
+
+    private String password;
+
+    private boolean emailVerified;
+
+    private String emailCheckToken;
+
+    private LocalDateTime joinedAt;
+
+    private String bio;
+
+    private String url;
+
+    private String occupation;
+
+    private String location;
+
+    @Lob @Basic(fetch = FetchType.EAGER)
+    private String profileImage;
+
+    // 알림설정
+    private boolean studyCreatedByEmail;
+
+    private boolean studyCreatedByWeb;
+
+    private boolean studyEnrollmentResultByEmail;
+
+    private boolean studyEnrollmentResultByWeb;
+
+    private boolean studyUpdatedByEmail;
+
+    private boolean studyUpdateByWeb;
+}
+
